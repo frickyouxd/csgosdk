dc9b7ed2ef0e9e2398a4354a7ca418506574bf3f
Charlie <privste.private.email@gmail.com>
2017-12-02 22:23:56 -0600

    csgo sdk
    csgo sdk

---
+++ csgo-sdk/csgo-sdk.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define _HAS_CXX17 1 // c++17
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Common.h"
+#include "sdk/SDK.h"
+
+#include "hack/Offsets.h"
+#include "hack/Basics/NetvarManager.h"
+#include "hack/Basics/CHook.h"
+#include "hack/Global.h"
+#include "hack/Basics/Util.h"
+#include "hack/Basics/Math.h"
+#include "hack/Hooks.h"
+#include "hack/Basics/Config.h"
+#include "hack/Basics/ConfigLoader.h"
+
+#include "hack/Helpers/Prediction.h"
+#include "hack/Helpers/Autowall.h"
+#include "hack/Helpers/DrawManager.h"
+#include "hack/Helpers/Backtracking.h"
+#include "hack/Helpers/Lagcompensation.h"
+#include "hack/Helpers/Resolver.h"
+#include "hack/Helpers/GrenadePrediction.h"
+#include "hack/Helpers/Antiaim.h"
+
+#include "hack/Components/Aimbot.h"
+#include "hack/Components/Miscellaneous.h"
+#include "hack/Components/Visuals.h"
